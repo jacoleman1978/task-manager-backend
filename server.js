839ba6f8db5162a88224ac7d6d3b1016c1f6933b
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-02-21 13:44:58 -0600

    Added server.js to build app

---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependencies
+import express from 'express';
+import cors from 'cors';
+
+import tasks from './controllers/tasks_router.js';
+
+// Application
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/schedules', tasks);
+
+app.use('*', (req,res) => {
+    res.status(404).json({error: "Page not found"})
+})
+
+export default app;
